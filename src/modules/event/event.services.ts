fea8368a905a0799af699c89582a9a1d7ecd6a2e
minatisleeping <minatt2002@gmail.com>
2024-05-27 13:52:14 +0700

    Fix: config response..

--- src/modules/event/event.services.ts
+++ src/modules/event/event.services.ts
@@ -2,6 +2,9 @@ import databaseService from '~/database/database.services'
 import { EventRequestBody } from './event.requests'
 import { ObjectId } from 'mongodb'
 import Event from './event.schema'
+import { env } from '~/config/environment'
+import e from 'express'
+import { cloneDeep } from 'lodash'
 
 class EventService {
   async createEvent(user_id: string, body: EventRequestBody) {
@@ -24,25 +27,33 @@ class EventService {
       databaseService.events
         .aggregate([
           {
-            $skip: (page - 1) * limit
+            $lookup: {
+              from: env.DB_COLLECTION_EVENT_OPERATORS,
+              localField: 'event_operator_id',
+              foreignField: '_id',
+              as: 'event_operator'
+            }
           },
+          { $skip: (page - 1) * limit },
+          { $limit: limit },
           {
-            $limit: limit
+            $project: {
+              event_operator_id: 0,
+              event_operator: { password: 0, created_at: 0 }
+            }
           }
         ])
-        .toArray(),
+        .toArray()
+        .then((events) => {
+          return events.map((event) => {
+            return {
+              ...event,
+              event_operator: event.event_operator[0]
+            }
+          })
+        }),
       databaseService.events
-        .aggregate([
-          {
-            $skip: (page - 1) * limit
-          },
-          {
-            $limit: limit
-          },
-          {
-            $count: 'total'
-          }
-        ])
+        .aggregate([{ $skip: (page - 1) * limit }, { $limit: limit }, { $count: 'total' }])
         .toArray(),
       this.getAllEventList()
     ])
