6cf48fd35e79a9e3a5a2ebd017b796112d4f131c
hoangday185 <jarvietnguyen318@gmail.com>
2024-06-11 14:48:55 +0700

    fix : cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes'
 import { env } from './environment'
 
 export const corsOption = {
-  origin: env.PRODUCTION_FRONTEND_URL || 'localhost:3000',
+  origin: '*',
   credentials: true, // access-control-allow-credentials:true
   allowedHeaders: ['Content-Type', 'Authorization'], // access-control-allow-headers
   optionSuccessStatus: StatusCodes.OK
