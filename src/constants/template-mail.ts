37695c0fdafb4d87de0bd61bb3c93b42b9be103e
minatisleeping <minatt2002@gmail.com>
2024-06-16 08:47:49 +0700

    fix: redirect home page when user verify their acc..

--- src/constants/template-mail.ts
+++ src/constants/template-mail.ts
@@ -134,7 +134,7 @@ export const templateForgotPassword = (email: string, forgot_password_token: str
                       background-color: #ff942b;
                       text-decoration: none;
                       transition: all 0.2s ease;">
-                      <a style="text-decoration: none; color: white" href="${env.PRODUCTION_FRONTEND_URL}/users/verify-forgot-password-token?forgot_password_token=${forgot_password_token}">Reset Password</a>
+                      <a style="text-decoration: none; color: white" href="http://localhost:4000/users/verify-forgot-password-token?forgot_password_token=${forgot_password_token}">Reset Password</a>
                     </button>
                 </div>
             </div>
