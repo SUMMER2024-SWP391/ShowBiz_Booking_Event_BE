9dc5d34a89e715ff35a5ae600a67d4d33f6fcd76
minatisleeping <minatt2002@gmail.com>
2024-06-21 14:14:09 +0700

    fix: validate regex time..

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -1,5 +1,5 @@
 export const REGEX_DATE = /^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\d{4})$/
-export const REGEX_TIME = /^(0[1-9]|1[0-2]):([0-5][0-9])\s?(AM|PM)$/i
+export const REGEX_TIME = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/
 export const REGEX_FPT_EMAIL = /^[a-zA-Z._%+-]+[0-9]{6}@fpt\.edu\.vn$/
 export const REGEX_FPT_EVENT_OPERATOR_EMAIL = /@fpt\.edu\.vn$/
 export const REGEX_PHONE_NUMBER_VIETNAM = /^(\+84|0)\d{9,10}$/
